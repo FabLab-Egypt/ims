e4dfd41ac8ba1abc8b6ae0e1476805337fa20179
Chehab Mustafa-Hilmy <m@chehab.me>
2014-04-14 16:21:28 +0200

    [NEW] community app, inital commit

--- fablab_ims/community/admin.py
+++ fablab_ims/community/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from community.models import Member, Volunteer, Visitor
 
-# Register your models here.
+admin.site.register(Member)
+admin.site.register(Volunteer)
+admin.site.register(Visitor)
--- fablab_ims/community/models.py
+++ fablab_ims/community/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class IMS_Managers(models.Model):
+class IMS_Manager(models.Model):
     user = models.ForeignKey(User, unique=True)
 
-class Characters(models.Model):
+class Character(models.Model):
     name      = models.CharField(max_length = 100, verbose_name = "Full Name")
     email     = models.CharField(max_length = 100, verbose_name = "email")
     mobile    = models.CharField(max_length = 20, verbose_name = "mobile")
     birhtdate = models.DateField(auto_now=False, auto_now_add=False)
     # professsion
     
-    
+class Member(Character):
+    member_id = models.CharField(max_length = 40, verbose_name = "Member ID") # sha1(email;yyyy.mm.dd;mobile)
+
+class Volunteer(Member):
+    pass
+
+class Visitor(Character):
+    pass
